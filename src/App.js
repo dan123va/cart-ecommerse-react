713bf325b1e616df1b3b321442c42232afed63fa
Daniel Silverio <daniel.silverio@mariachi.io>
2022-07-06 15:49:28 -0500

    Se agrego el hook para usar cualquier peticion a la api y se creo el componente para mostrar productos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+import useFetch from "./hooks/useFetch";
 import TopMenu from "./components/TopMenu";
+import Products from "./components/Products";
+import { urlApiProducts } from "./utils/constants";
 
 function App() {
+  const products = useFetch(urlApiProducts);
+  
   return (
     <div className="App">
       <TopMenu />
+      <Products products={products}/>
     </div>
   );
 }
